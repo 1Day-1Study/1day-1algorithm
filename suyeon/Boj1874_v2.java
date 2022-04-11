c141a64d90e28ce2d07f73a5f63b98363ec1620d
ArchivvonJang <76926509+ArchivvonJang@users.noreply.github.com>
2022-04-11 20:16:34 +0900

    rename

--- suyeon/Boj1874_v2.java
+++ suyeon/Boj1874_v2.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class Bob1874_v2 {
+public class Boj1874_v2 {
 
     public static void main(String args[]) throws IOException {
 
