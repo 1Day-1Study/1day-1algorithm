c8d2cfd64f2a06e451e51d9f510cf9afcd7a75f4
ArchivvonJang <76926509+ArchivvonJang@users.noreply.github.com>
2022-04-14 00:23:41 +0900

    feat: add Boj1158

---
+++ suyeon/Boj1158.java
@@ -0,0 +1,45 @@
+package suyeon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Boj1158 {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String[] testcase = br.readLine().split(" ");
+
+        int N = Integer.parseInt(testcase[0]);
+        int K = Integer.parseInt(testcase[1]);
+
+        Queue<Integer> que = new LinkedList<>();
+
+        for (int i = 0; i < N; i++) {
+            que.offer(i+1); //for문은 0부터 시작하므로 +1해준다.
+        }
+
+        sb.append("<");
+
+        //que 크기가 1일 때까지 반복
+        while(que.size() != 1){
+            //k-1(가장 맨 직전값)까지 맨 뒤로 보낸다
+            for (int i = 0; i < K-1; i++) {
+                que.offer(que.poll());
+
+            }
+            sb.append(que.poll()+", ");
+        }
+        //que 사이즈는 1일때 마지막 남은 값 poll
+        sb.append(que.poll() + ">");
+
+
+        System.out.println(sb.toString());
+
+
+    }
+}
