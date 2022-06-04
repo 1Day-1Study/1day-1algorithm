49574539adb181ede3145c62744eba0e28e59fe7
HyunJay52 <hyunjungk52@gmail.com>
2022-06-04 16:47:55 +0900

    done 1253

--- hyunJung/two_pointer/Good_1253.java
+++ hyunJung/two_pointer/Good_1253.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Good_1253 {
@@ -15,30 +16,34 @@ public class Good_1253 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		N = Integer.parseInt(br.readLine());
-		A = new int[N];
+		A = new int[N+1];
 		
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int cnt = 0;
-		while(N-- > 0) {
-			A[cnt] = Integer.parseInt(st.nextToken());
-			cnt++;
+		for(int i = 1; i <= N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		br.close();
+		Arrays.sort(A, 1, N+1);
 		
-		for(int i=0; i<N; i++) {
+		for(int i = 1; i <= N; i++) {
 			if(two_pointer(i)) answer++;
 		}
 		
 		System.out.println(answer);
+		br.close();
 	}
 	
 	static boolean two_pointer(int idx) {
-		int left = 0, right = 0, target = A[idx];
+		int left = 1, right = N, target = A[idx];
 	
 		while(left < right) {
-			
-			if(A[left] + A[right] == target) return true;
+			if(left == idx) left++;
+			else if(right == idx) right--;
+			else {
+				if(A[left] + A[right] < target) left ++;
+				else if(A[left] + A[right] == target) return true;
+				else right --;
+			}
 		}
 		
 		return false;
